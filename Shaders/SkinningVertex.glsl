f8254cfa36d395b39f66bc48423564e402ec9293
SamBrumskill713 <C5055756@newcastle.ac.uk>
2025-11-03 16:27:00 +0000

    tut 10 done

--- Shaders/SkinningVertex.glsl
+++ Shaders/SkinningVertex.glsl
@@ -7,15 +7,19 @@ uniform mat4 projMatrix;
 in vec3 position;
 in vec2 texCoord;
 in vec4 jointWeights;
+in vec4 jointColour;
 in ivec4 jointIndices;
+
 
 uniform mat4 joints[128];
 
 out Vertex{
 	vec2 texCoord;
+	vec4 jointColour;
 } OUT;
 
 void main(void){
+	vec3 visColour = vec3(0.0);
 	vec4 localPos = vec4(position, 1.0f);
 	vec4 skelPos = vec4(0, 0, 0, 0);
 
@@ -24,8 +28,15 @@ void main(void){
 		float jointWeight = jointWeights[i];
 
 		skelPos += joints[jointIndex] * localPos * jointWeight;
+		if(jointWeight == 1){
+			visColour = vec3(1.0, 0.0, 0.0);
+		}
+		else{
+			visColour = vec3(0.0, 0.0, 1.0);
+		}
 	}
 	mat4 mvp = projMatrix * viewMatrix * modelMatrix;
 	gl_Position = mvp * vec4(skelPos.xyz, 1.0);
 	OUT.texCoord = texCoord;
+	OUT.jointColour = vec4(visColour, 1.0);
 }
--- Shaders/basicVertex.glsl
+++ Shaders/basicVertex.glsl
@@ -10,4 +10,4 @@ out Vertex{
 void main(void){
 	gl_Position = vec4(position, 1.0);
 	OUT.colour = colour;
-}
+}	
